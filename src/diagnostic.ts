acf960df2fc61c1e7c32fada8433416ba758e842
GHOST <ghostdevbusiness@gmail.com>
2024-07-09 15:08:48 +0100

    feat: initial diagnostics parsing

---
+++ src/diagnostic.ts
@@ -0,0 +1,70 @@
+// Based on MIT Licenced code from SvelteLab
+// https://github.com/SvelteLab/SvelteLab/blob/a3fb823356a9ed1d16eb8535340c9813b7eb547d/src/lib/stores/editor_errors_store.ts#L19
+// https://github.com/SvelteLab/SvelteLab/blob/a3fb823356a9ed1d16eb8535340c9813b7eb547d/src/lib/webcontainer.ts#L301
+
+import { exec } from 'node:child_process';
+import { join } from 'node:path/posix';
+import { z } from 'zod';
+
+const diagnosticSchema = z.object({
+	type: z
+		.string()
+		.toLowerCase()
+		.refine((type): type is 'error' | 'warning' =>
+			['error', 'warning'].includes(type),
+		),
+	filename: z.string().transform((filename) => `./${filename}`),
+	start: z.object({
+		line: z.number().transform((line) => line + 1),
+		character: z.number(),
+	}),
+	end: z.object({
+		line: z.number().transform((line) => line + 1),
+		character: z.number(),
+	}),
+	message: z.string(),
+	code: z.union([z.number(), z.string()]).optional(),
+	source: z.string().optional(),
+});
+
+type RawDiagnostic = z.infer<typeof diagnosticSchema>;
+
+export interface Diagnostic extends Omit<RawDiagnostic, 'filename'> {
+	fileName: string;
+	path: string;
+}
+
+export async function get_diagnostics(cwd: string) {
+	return new Promise<Diagnostic[]>((resolve) => {
+		exec(
+			'pnpm exec svelte-check --output machine-verbose',
+			{ cwd },
+			(error, stdout, stderr) => {
+				const lines = [...stdout.split('\n'), ...stderr.split('\n')];
+				const diagnostics: Diagnostic[] = [];
+
+				for (const line of lines) {
+					const result = line
+						.trim()
+						.match(/^\d+\s(?<diagnostic>.*)$/);
+
+					if (result && result.groups) {
+						try {
+							const raw = JSON.parse(result.groups.diagnostic);
+							const { filename, ...diagnostic } =
+								diagnosticSchema.parse(raw);
+
+							diagnostics.push({
+								...diagnostic,
+								fileName: filename,
+								path: join(cwd, filename),
+							});
+						} catch (e) {}
+					}
+				}
+
+				resolve(diagnostics);
+			},
+		);
+	});
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello World');
+import { get_diagnostics } from './diagnostic';
+import { relative } from 'node:path/posix';
+import cardinal from 'cardinal';
+
+const TARGET = '';
+
+const diagnostics = await get_diagnostics(TARGET);
+
+for (const diagnostic of diagnostics) {
+	console.log(
+		`[${diagnostic.type.toUpperCase()}]`,
+		// prettier-ignore
+		`${relative(TARGET, diagnostic.path)}:${diagnostic.start.line}:${diagnostic.start.character}\n`,
+		cardinal.highlight(diagnostic.message),
+		'\n\n',
+	);
+}
+
+console.log('results', diagnostics.length);
