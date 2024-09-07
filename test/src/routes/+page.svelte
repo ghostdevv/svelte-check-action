3d0f223c769a98a1bb2c2ace5933bad9c720e896
GHOST <ghostdevbusiness@gmail.com>
2024-09-07 23:05:07 +0100

    tmp

--- test/src/routes/+page.svelte
+++ test/src/routes/+page.svelte
@@ -1,13 +1,1 @@
-<script lang="ts">
-	let something: string = 1;
-</script>
-
-<div on:click={() => {}}></div>
-
-<input autofocus />
-
-<div
-	on:mouseenter={() => {
-		console.log('something');
-	}}>
-</div>
+<p>hello world</p>
