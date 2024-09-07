dfff3acfc93c52599b65786b309aa8f058841ed3
GHOST <ghostdevbusiness@gmail.com>
2024-09-07 23:05:58 +0100

    Revert "tmp"
    This reverts commit 3d0f223c769a98a1bb2c2ace5933bad9c720e896.

--- test/src/routes/+page.svelte
+++ test/src/routes/+page.svelte
@@ -1,1 +1,13 @@
-<p>hello world</p>
+<script lang="ts">
+	let something: string = 1;
+</script>
+
+<div on:click={() => {}}></div>
+
+<input autofocus />
+
+<div
+	on:mouseenter={() => {
+		console.log('something');
+	}}>
+</div>
